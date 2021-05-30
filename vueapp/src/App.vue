6f3b0272e267abe250c030a73547b58891c0ea2e
Sewmini Dissanayaka <61307442+Sewmini-18@users.noreply.github.com>
2021-05-30 11:01:16 +0530

    first commit

---
+++ vueapp/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+     <navbar></navbar>
+    <h1 id="title1">Add Product List</h1>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+export default {
+  components:{
+       'navbar':NavBar
+   }
+};
+</script>
+
+<style>
+#title1 {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ vueapp/src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template name="navbar">
+  <nav class="navbar navbar-expand-lg navbar-light bg-warning" >
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img
+          src="/docs/5.0/assets/brand/bootstrap-logo.svg"
+          alt=""
+          width="30"
+          height="24"
+        />
+      </a>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Product List</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Add Product</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-dark" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
